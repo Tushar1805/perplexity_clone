c8814a6ad6d636aa9cad87afee25da559e9c7c33
Tushar Kalbhande <tusharkalbhande1805@gmail.com>
2025-09-01 20:37:39 +0530

    App name change

--- lib/core/resources/shared_app_strings.dart
+++ lib/core/resources/shared_app_strings.dart
@@ -1,7 +1,7 @@
 class SharedAppStrings {
   static const String internetError = 'Error in internet connection';
   static const String retry = 'Retry';
-  static const String appNameString = 'GlobePorter';
+  static const String appNameString = 'Perplexity Clone';
   static const noInternetString = 'Oops! No Internet Connection';
   static const someErrorOccurredString = 'Some Error Occurred';
 }
